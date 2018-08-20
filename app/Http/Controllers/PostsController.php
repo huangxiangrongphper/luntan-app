595dda1e6c458e098e3eb4b7e6efcf5b1b50f0d2
Flourishing <huangxiangrong827@163.com>
2018-08-21 01:19:37 +0800

    实现一些用户权限相关功能

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -80,7 +80,11 @@ class PostsController extends Controller
      */
     public function edit($id)
     {
-        //
+        $discussion = Discussion::findOrFail($id);
+        if(\Auth::user()->id !== $discussion->user_id){
+            return redirect('/');
+        }
+        return view('forum.edit',compact('discussion'));
     }
 
     /**
@@ -90,9 +94,11 @@ class PostsController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Requests\StoreBlogPostRequest $request, $id)
     {
-        //
+        $discussion = Discussion::findOrFail($id);
+        $discussion->update($request->all());
+        return redirect()->action('PostsController@show',['id'=>$discussion->id]);
     }
 
     /**
